618b2a1cc72122ec70d91456427e74ee17b46da8
NaszeGryn <70233490+NaszeGryn@users.noreply.github.com>
2021-06-20 15:12:22 +0200

    Add files via upload

---
+++ VL/VL.cpp
@@ -0,0 +1,170 @@
+#include "VLMain.h"
+
+namespace VL {
+	void VL_file::load(const std::string& VL_S_Location, const std::string& Name) {
+	
+		std::vector<std::string>* a = new std::vector<std::string>();
+		std::fstream stream;
+		if (Name == "") {
+			this->filename = VL_S_Location;
+		}
+		else {
+			this->filename = Name;
+		}
+
+		stream.open(VL_S_Location, std::ios::in);
+		if (stream.good()) {
+			std::string str;
+			while (std::getline(stream, str)) {
+				a->push_back(str);
+			}
+
+			// checking all lines
+			for (int i = 0; i < a->size(); i++) {
+
+				std::string line = (*a)[i];
+				std::string l_name = "";
+
+				std::string l_value = "";
+				if (line.length() > 0) {
+					if (line[0] != '#') {
+						// finding '=' in line
+						/* starts with \ */
+
+						if (line[0] == 0x5C) {
+							this->V_list[this->V_list.size() - 1].value += " " + line.substr(1, line.size() - 1);
+						}
+						else if (line[0] == '&') {
+							this->V_list[this->V_list.size() - 1].value += "\n" + line.substr(1, line.size() - 1);
+						}
+						else {
+							// finding '=' in line
+							if (line.find('=') < line.length()) {
+								int eqPos = strcspn(line.c_str(), "=");
+								l_name = line.substr(0, eqPos);
+								l_value = line.substr(eqPos + 1, line.size() - (eqPos + 1));								
+							}
+							else {
+								l_name = line;
+							}
+							bool repeat = 0;
+							for (int j = 0; j < this->V_list.size(); j++) {
+								if (this->V_list[j].title == l_name) {
+									// podmiana wartosci
+									this->V_list[j].value = l_value;
+									repeat = true;
+								}
+
+							}
+							if (!repeat) {
+								V_list.push_back(VL_variable(l_name, l_value));
+							}
+						}
+					}
+				}
+			}
+
+		}
+		
+		stream.close();
+	
+		delete a;
+		a = NULL;
+	}
+	// ----- search functions -----
+
+	std::vector<VL_variable>* VL_file::getList() {
+		return &this->V_list;
+	}
+
+	std::string VL_file::getName() {
+		return this->filename;
+	}
+
+	std::string VL_file::getValue(const std::string& name) {
+		for (int i = 0; i < V_list.size(); i++) {
+			if (V_list[i].title == name)
+				return V_list[i].value;
+		}
+		return "";
+	}
+
+	std::string VL_file::getValue(int index) {
+		if (index < V_list.size())
+			return V_list[index].value;
+		return "";
+	}
+
+	int VL_file::getIndex(const std::string& name) {
+		for (int i = 0; i < V_list.size(); i++) {
+			if (V_list[i].title == name)
+				return i;
+		}
+		return -1;
+	}
+
+	VL_variable* VL_file::getVariable(int index) {
+		if (index < V_list.size())
+			return &V_list[index];
+		return NULL;
+	}
+	VL_variable* VL_file::getVariable(const std::string& name) {
+		for (int i = 0; i < V_list.size(); i++) {
+			if (V_list[i].title == name)
+				return &V_list[i];
+		}
+		return NULL;
+	}
+
+	// ======================= HANDLER =======================
+
+
+
+	// utility functions
+	std::vector<VL_variable>* VL_Handler::getList() {
+		return &this->V_list;
+	}
+
+	void VL_Handler::add(VL_variable* variable) {
+		bool sorted = false;
+		V_list.push_back(*variable);
+		
+	}
+
+	std::string VL_Handler::getValue(const std::string& name) {
+		for (int i = 0; i < V_list.size(); i++) {
+			if (V_list[i].title == name)
+				return V_list[i].value;
+		}
+		return "";
+	}
+
+	std::string VL_Handler::getValue(int index) {
+		if (index < V_list.size())
+			return V_list[index].value;
+		return "";
+	}
+
+	int VL_Handler::getIndex(const std::string& name) {
+		for (int i = 0; i < V_list.size(); i++) {
+			if (V_list[i].title == name)
+				return i;
+		}
+		return -1;
+	}
+
+	VL_variable* VL_Handler::getVariable(int index) {
+		if (index < V_list.size())
+			return &V_list[index];
+		return NULL;
+	}
+	VL_variable* VL_Handler::getVariable(const std::string& name) {
+		for (int i = 0; i < V_list.size(); i++) {
+			if (V_list[i].title == name)
+				return &V_list[i];
+		}
+		return NULL;
+	}
+
+
+}
---
+++ VL/VLMain.h
@@ -0,0 +1,169 @@
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+namespace VL {
+	// Variable struct
+	struct VL_variable {
+		// constructor
+		VL_variable(const std::string& title, const std::string& value) {
+			this->title = title;
+			this->value = value;
+		}
+
+		// variables
+		std::string title;
+		std::string value;
+	};
+
+
+	// varialbe language file 
+	class VL_file {
+	public:
+		//constructor
+		VL_file(const std::string& VL_S_Location, const std::string& Name = "") {
+			this->load(VL_S_Location, Name);
+		}
+
+		// interpreter
+		void load(const std::string& VL_S_Location, const std::string& Name = "");
+
+		// ---- search functions ---- 
+		std::string getName();
+
+		std::string getValue(const std::string& name);
+
+		std::string getValue(int index);
+
+		int getIndex(const std::string& name);
+
+		VL_variable* getVariable(int index);
+		VL_variable* getVariable(const std::string& name);
+
+		std::vector<VL_variable>* getList();
+	public:
+		std::vector<VL_variable> V_list;
+		std::string filename;
+		
+	};
+
+	// combines variables from file to variable list
+	static void combine(std::vector<VL_variable>* V_list, VL_file* file) {
+		for (int i = 0; i < file->V_list.size(); i++) {
+
+			bool repeat = 0;
+			for (int j = 0; j < V_list->size(); j++) {
+				if ((*V_list)[j].title == file->V_list[i].title) {
+					(*V_list)[j].value = file->V_list[i].value;
+					repeat = true;
+				}
+
+			}
+
+			if (!repeat) {
+				V_list->push_back(file->V_list[i]);
+			}
+		}
+	}
+
+	static void saveToFile(VL_file& file, const std::string& filename, bool clear_file) {
+		std::fstream stream;
+		if (clear_file)
+			stream.open(filename, std::ios::out | std::ios::trunc);
+		else
+			stream.open(filename, std::ios::out);
+
+		for (int i = 0; i < file.V_list.size(); i++) {
+			stream << file.V_list[i].title;
+			if (file.V_list[i].value != "") {
+				stream << "=";
+				for (int j = 0; j < file.V_list[i].value.size(); j++) {
+					stream << file.V_list[i].value[j];
+					if (file.V_list[i].value[j] == '\n') {
+						stream << '&';
+					}
+				}
+			}
+			stream << "\n";
+		}
+
+		stream.close();
+	}
+
+	
+
+	// variable handler
+	class VL_Handler {
+	public:
+		VL_Handler() { ; }
+		~VL_Handler() {
+			V_list.clear();
+		}
+
+		// adds variables from file
+		void addFromFile(const std::string& VL_S_Location) {
+			VL_file* tempFile = new VL_file(VL_S_Location);
+			VL::combine(&V_list, tempFile);
+			delete tempFile;
+			tempFile = NULL;
+		}
+
+
+		// adds variable
+		void add(VL_variable* variable);
+
+		std::string getValue(const std::string& name);
+
+		std::string getValue(int index);
+		int getIndex(const std::string& name);
+
+		VL_variable* getVariable(int index);
+		VL_variable* getVariable(const std::string& name);
+
+
+		std::vector<VL_variable>* getList();
+	public:
+
+		std::vector<VL_variable> V_list;
+	};
+
+	static void saveToFile(VL_Handler& file, const std::string& filename, bool clear_file) {
+		std::fstream stream;
+		if (clear_file)
+			stream.open(filename, std::ios::out | std::ios::trunc);
+		else
+			stream.open(filename, std::ios::out);
+
+		for (int i = 0; i < file.V_list.size(); i++) {
+			stream << file.V_list[i].title;
+			if (file.V_list[i].value != "") {
+				stream << "=";
+				for (int j = 0; j < file.V_list[i].value.size(); j++) {
+					stream << file.V_list[i].value[j];
+					if (file.V_list[i].value[j] == '\n') {
+						stream << '&';
+					}
+				}
+			}
+			stream << "\n";
+		}
+
+		stream.close();
+	}
+
+	class VL_FileHandler {
+	public:
+		VL_FileHandler();
+		~VL_FileHandler();
+
+		void addFile(VL_file& file);
+		void addFile(const std::string& VL_S_Location, const std::string& Name = "");
+
+		VL_file* getFile(const std::string& name);
+		VL_file* getFile(int index);
+
+		std::vector<VL_file>* getList();
+	private:
+		std::vector<VL_file> files;
+	};
+}
